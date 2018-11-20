5ec081b4c0de52deafcbaa2b5b1389e24836e48e
phantomofparis <phantomofparis@gmail.com>
2018-11-20 18:47:19 +0530

    made it better

--- dvt.sh
+++ dvt.sh
@@ -2,6 +2,52 @@
 
 # reads a list of image file path and creates corresponding thumbs
 # the file concerned is just absolute paths of image files and is supplied as an argument
+#usage arguments
+show_help(){
+
+    cat <<EOF
+ Usage: ${0##*/} [-h] 
+ Reduce the size of image files like png and jpg
+
+     -h          display this help and exit
+
+EOF
+ }
+
+check_program_arguments(){
+     is_this_null=$1
+     if [ -z $is_this_null ]
+     then
+	 # ok, problem, there was no url file supplied
+	 # echo out error message tell them to supply a url file
+	 echo "I need a text file having full path of images in each line"
+     else
+	 if [ -f $is_this_null ]
+	 then
+	     #ok a file that exists has been supplied
+	     create_thumbs	     
+	     #	 parse_program_arguments $is_this_null
+	 else
+	     parse_program_arguments $is_this_null
+	     fi
+     fi
+ }
+
+ parse_program_arguments(){
+ while getopts h: opt; do
+     case $opt in
+         h)
+             show_help
+             exit 0
+             ;;
+          *) echo "Invalid arguments"
+             show_help >&2
+             exit 1
+             ;;
+     esac
+ done
+# shift "$((OPTIND-1))"   # Discard the options and sentinel --
+}
 
 counter=1000
 create_thumbs(){
@@ -17,6 +63,7 @@ counter=$(expr $counter + 1)
 done
 }<$1
 
-create_thumbs $*
+#create_thumbs $*
 
 
+check_program_arguments $*
